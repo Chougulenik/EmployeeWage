a30fd29905f328ecfe65b10482595075571b3e75
Chougulenik <nikitachougule47@gmail.com>
2022-03-02 16:09:03 -0500

    UC-1 added

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -4,7 +4,16 @@ public class EmployeeWage {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome Employee Wage Computation Program");
+
+	     System.out.println("Welcome Employee Wage Computation Program");
+	
+  		int Full_time = 1;
+        int empCheck = (int)Math.floor(Math.random() *10) % 2;
+        if ( empCheck == Full_time )
+                System.out.println("employee present");
+        else
+               System.out.println("employee not present");
+
 
 	}
 
