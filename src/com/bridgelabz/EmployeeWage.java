d9f5aa2025161fce1be2a0c18f13a43771be4ce9
Chougulenik <nikitachougule47@gmail.com>
2022-03-02 20:13:11 -0500

    UC-5 added

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,29 +1,39 @@
 package com.bridgelabz;
 
 public class EmployeeWage {
-	static final int Full_Time= 1;
-	static final int Part_Time= 2;
-	static final int Wage_Per_Hrs =20;
+	static final int FULL_TIME= 8;
+	static final int PART_TIME= 4;
+	static final int WAGE_PER_HRS =20;
+	static final int WORKING_DAY =20;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	     System.out.println("Welcome Employee Wage Computation Program");
 	
   		int empHrs = 0;
-  		//int empWage = 0;
- 
+  		int monthlyWage = 0;
+  		int days=1;
+  		while(days<=20) {
         int empCheck = (int)Math.floor(Math.random() *10) % 3;
         switch (empCheck) {
-        case Full_Time :
-                empHrs=8;
+        case 1 :
+        		System.out.println("Employee is Full time presnt");
+        		int dailyWage = FULL_TIME * WAGE_PER_HRS;
+                System.out.println("Daily wage : "+dailyWage);
+                monthlyWage = monthlyWage + dailyWage; 
                 break;
-        case Part_Time :
-        		empHrs=4;
-        		break;
+        case 2 :
+        	  	System.out.println("Employee is Part time presnt");
+        	  	int dailyWage_par = PART_TIME * WAGE_PER_HRS;
+        	  	System.out.println("Daily wage : "+ dailyWage_par);
+        	  	monthlyWage = monthlyWage + dailyWage_par; 
+        	  	break;
+        default :
+        		System.out.println("Employee not present");
         }		
-        int empWage = empHrs * Wage_Per_Hrs;
-        System.out.println("Emp Wage : " +empWage);
-
+        days ++;
+  		}
+  		System.out.println("Monthly Wage : " + monthlyWage);
 	}
 
 }
